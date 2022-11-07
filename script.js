dae3545f797dcb29c9813c1ab2a3b1fc838ba042
Moulisree Karmakar <moulisreek1441@gmail.com>
2022-11-07 19:50:13 +0530

    first commit

--- script.js
+++ script.js
@@ -4,11 +4,29 @@ function addNewWEField() {
 let newnode = document.createElement("textarea");
 newnode.classList.add("form-control");
 newnode.classList.add("weField");
-newnode.setAttribute("rows",3);
+newnode.classList.add("mt-2");
+newnode.setAttribute("rows", 3);
+newnode.setAttribute("placeholder" , "Enter here");
 
 let weOb = document.getElementById("we");
 let weAddButtonOb = document.getElementById("weAddButton");
 
 weOb.insertBefore(newnode , weAddButtonOb);
+
+}
+
+function addNewAQField() {
+
+    let newnode = document.createElement("textarea");
+    newnode.classList.add("form-control");
+    newnode.classList.add("eqField");
+    newnode.classList.add("mt-2");
+    newnode.setAttribute("rows", 3);
+    newnode.setAttribute("placeholder" , "Enter here");
+    
+    let aqOb = document.getElementById("aq");
+    let aqAddButtonOb = document.getElementById("weAddButton");
+    
+    aqOb.insertBefore(newnode , aqAddButtonOb);
 
 }
